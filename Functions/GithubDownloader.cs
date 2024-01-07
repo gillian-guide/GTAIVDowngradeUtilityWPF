b94fb6a1c1462083887b85f4620d9ee113909d45
AAAAAXfda <117472564+AAAAAXfda@users.noreply.github.com>
2024-01-07 08:28:34 -0500

    Code enhancement (#1)
    * Slight cleanup in GithubDownloader.cs

--- Functions/GithubDownloader.cs
+++ Functions/GithubDownloader.cs
@@ -1,33 +1,29 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Net;
 
 namespace GTAIVSetupUtilityWPF.Functions
 {
-
-    // hi here, i'm an awful coder, so please clean up for me if it really bothers you
-#pragma warning disable S101 // Types should be named in PascalCase
     public static class GithubDownloader
-#pragma warning restore S101 // Types should be named in PascalCase
     {
-
         private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+        
         public static void Download(string downloadUrl, string destination, string downloadedName)
         {
             try
             {
                 Logger.Debug(" Downloading the selected release...");
-                using (WebClient client = new())
+                using (WebClient client = new WebClient())
                 {
-                    client.DownloadFile(downloadUrl, $"{destination}\\{downloadedName}");
+                    client.DownloadFile(downloadUrl, Path.Combine(destination, downloadedName));
                 }
-
                 Logger.Debug(" Successfully downloaded.");
             }
             catch (Exception ex)
             {
-                Logger.Debug(ex, "Error downloading");
+                Logger.Error(ex, "Error downloading");
+                throw; // Let the error propagate, no need to hide it.
             }
         }
     }
